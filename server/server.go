683bc0005aafa3fe3fea6c4abf4d6bb1087ef967
c0de_runn3r <mikitenko2001@gmail.com>
2024-02-27 08:35:12 +0100

    error fix

--- server/server.go
+++ server/server.go
@@ -35,8 +35,7 @@ func StartServer(db *storage.PosgresStore) {
 	// Google Drive
 	google.GET("/drive-to-storj/:ID", handleSendFileFromGoogleDriveToStorj)
 	google.GET("/storj-to-drive/:name", handleSendFileFromStorjToGoogleDrive)
-	google.GET("/drive-get-file-names", googlepack.GetFileNames)
-	google.GET("/google-auth", googlepack.Autentificate)
+	google.GET("/drive-get-file-names", handleGetGoogleDriveFileNames)
 	google.GET("/drive-get-file/:ID", googlepack.GetFileByID)
 	google.GET("/all-drive-to-storj", handleSendAllFilesFromGoogleDriveToStorj)
 
@@ -45,6 +44,21 @@ func StartServer(db *storage.PosgresStore) {
 	google.GET("/photos-list-photos-in-album/:ID", handleListPhotosInAlbum)
 	google.GET("/storj-to-photos/:name", handleSendFileFromStorjToGooglePhotos)
 	google.GET("/photos-to-storj/:ID", handleSendFileFromGooglePhotosToStorj)
+	google.GET("/all-photos-from-album-to-storj/:ID", handleSendAllFilesFromGooglePhotosToStorj)
+
+	// Gmail
+	google.GET("/gmail-list-threads", handleGmailGetThreads)
+	google.GET("/gmail-list-messages", handleGmailGetMessages)
+	google.GET("/gmail-get-message/:ID", handleGmailGetMessage)
+	google.GET("/gmail-message-to-storj/:ID", handleGmailMessageToStorj)
+	google.GET("/get-gmail-db-from-storj", handleGetGmailDBFromStorj)
+
+	// Google Cloud Storage
+	google.GET("/storage-list-buckets/:projectName", handleStorageListBuckets)
+	google.GET("/storage-list-items/:bucketName", handleStorageListObjects)
+	google.GET("/storage-item-to-storj/:bucketName/:itemName", handleGoogleCloudItemToStorj)
+	google.GET("/storage-item-from-storj-to-google-cloud/:bucketName/:itemName", handleStorjToGoogleCloud)
+	google.GET("/storage-all-items-to-storj/:bucketName", handleAllFilesFromGoogleCloudBucketToStorj)
 
 	// Dropbox
 	dropbox := e.Group("/dropbox")
