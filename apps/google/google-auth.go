c1ee4505e1da4b5910fda04ae32f0e3f2e866969
Pradip Parmar <prince.soamedia@gmail.com>
2024-10-21 17:22:34 +0530

    statement print

--- apps/google/google-auth.go
+++ apps/google/google-auth.go
@@ -315,7 +315,12 @@ func GetGoogleAccountDetailsFromAccessToken(accessToken string) (*GoogleAuthResp
 	defer resp.Body.Close()
 
 	var tokenInfo GoogleAuthResponse
-	if err := json.NewDecoder(resp.Body).Decode(&tokenInfo); err != nil {
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading response body: %v", err)
+	}
+	fmt.Println("Response body:", string(body))
+	if err := json.NewDecoder(bytes.NewBuffer(body)).Decode(&tokenInfo); err != nil {
 		return nil, fmt.Errorf("error decoding response: %v", err)
 	}
 
