5109d6ab27bd140c76001312d407095ac92b0bf5
Pradip Parmar <prince.soamedia@gmail.com>
2024-10-26 09:25:16 +0530

    added handling for uplink permission failed

--- server/server.go
+++ server/server.go
@@ -36,7 +36,7 @@ func StartServer(db *storage.PosgresStore, address string) {
 	job.GET("/", handleAutomaticSyncListForUser)
 	job.GET("/:job_id", handleAutomaticSyncDetails)
 	job.POST("/", handleAutomaticSyncCreate)
-	job.PUT("/:job_id", handleAutomaticSyncUpdate)
+	job.PUT("/:job_id", handleAutomaticBackupUpdate)
 	job.DELETE("/:job_id", handleAutomaticSyncDelete)
 
 	job.GET("/interval", handleIntervalOnConfig)
