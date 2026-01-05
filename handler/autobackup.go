58b2ca7fe888152b71531b6e98ef338bba00241f
Dhaval <dhavalder93@gmail.com>
2026-01-05 11:56:21 +0530

    Update backup interval values in autobackup.go to streamline monthly scheduling
    - Reduced the number of months in the monthly interval from 30 to 28 for improved accuracy.
    - Commented out the 'one_time' interval to clarify its current status in the configuration.

--- handler/autobackup.go
+++ handler/autobackup.go
@@ -26,10 +26,10 @@ var Err error
 var intervalValues = map[string][]string{
 	"monthly": {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13",
 		"14", "15", "16", "17", "18", "19", "20", "21", "22", "23",
-		"24", "25", "26", "27", "28", "29", "30"},
-	"weekly":   {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"},
-	"daily":    {"12am"},
-	"one_time": {},
+		"24", "25", "26", "27", "28"},
+	"weekly": {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"},
+	"daily":  {"12am"},
+	// "one_time": {},
 }
 
 type DatabaseConnection struct {
